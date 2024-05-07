12d5dbb98c3fd80451c635a2d026b1fbab64e5cf
rakib <hassanrakibul926@gmail.com>
2024-05-07 12:50:30 +0600

    product page responsive issue solv

--- shop/models.py
+++ shop/models.py
@@ -38,6 +38,10 @@ class Product(models.Model):
     def __str__(self) -> str:
         return self.name
     
+class ProductMoreImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='more image')
+    
     
 class Order(models.Model):
     customar_name = models.CharField(max_length=260)
